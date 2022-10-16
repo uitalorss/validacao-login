8bc4f1c2206ae9a683b736922a9fb768101e0d86
Uítalo <uitalo.urss@gmail.com>
2022-10-16 12:23:34 -0300

    implementação da funcionalidade de data e hora em tempo real

---
+++ assets/js/date.js
@@ -0,0 +1,16 @@
+(() => {
+  const infoRealTime = document.querySelector('[data-home="info-time"]')
+  const infoHour = document.createElement('h1');
+  const infoDate = document.createElement('p');
+
+  const showRealTime = setInterval(() => {
+    let date = new Date();
+    infoHour.classList.add('header-info__hour');
+    infoDate.classList.add('info-time--date');
+    infoHour.innerHTML = new Intl.DateTimeFormat('pt-br', {timeStyle: 'short'}).format(date);
+    infoDate.innerHTML = new Intl.DateTimeFormat('pt-br', {dateStyle: 'full'}).format(date);
+    infoRealTime.appendChild(infoHour);
+    infoRealTime.appendChild(infoDate);
+  }, 1000)
+  showRealTime;
+})()
