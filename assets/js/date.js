48f3b01bfe75578077d41ec513eccd561be858a2
Uítalo <uitalo.urss@gmail.com>
2022-10-17 00:25:11 -0300

    ajustes de responsividade para telas grandes, notebooks e tablets da tela home

--- assets/js/date.js
+++ assets/js/date.js
@@ -6,7 +6,7 @@
   const showRealTime = setInterval(() => {
     let date = new Date();
     infoHour.classList.add('header-info__hour');
-    infoDate.classList.add('info-time--date');
+    infoDate.classList.add('header-info__date');
     infoHour.innerHTML = new Intl.DateTimeFormat('pt-br', {timeStyle: 'short'}).format(date);
     infoDate.innerHTML = new Intl.DateTimeFormat('pt-br', {dateStyle: 'full'}).format(date);
     infoRealTime.appendChild(infoHour);
