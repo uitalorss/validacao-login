94f5ab8c5b8e692a0775ffb8ac6dabd4acc456b3
Uítalo <uitalo.urss@gmail.com>
2022-10-17 20:47:30 -0300

    estilizando o icone de usuario e senha  para entrar no input quando preenchido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
+let inputUser = document.querySelector('[data-form="user"]');
+let inputPassword = document.querySelector('[data-form="password"]');
+
 const entrar = (event) => {
   event.preventDefault();
-  let inputUser = document.querySelector('[data-form="user"]');
-  let inputPassword = document.querySelector('[data-form="password"]');
+
   let messageWarning = document.querySelector('[data-form="message-warning"]');
+
+
   userValid = {
     user: 'admin',
     password: 'admin'
@@ -25,8 +29,33 @@ const entrar = (event) => {
   }
 }
 
-function clearFields(){
-  inputUser.value = '';
-  inputPassword.value = '';
+  function clearFields(){
+    inputUser.value = '';
+    inputPassword.value = '';
+  }
+
+
+
 }
-}
+
+inputUser.addEventListener('input', (e) => {
+  const divUser = document.querySelector('[data-form="div-user"]')
+  if(e.target.value !== ''){
+    divUser.classList.remove("input-login__user");
+    divUser.classList.add("input-login__user--special")
+  }else{
+    divUser.classList.remove("input-login__user--special");
+    divUser.classList.add("input-login__user")
+  }
+})
+
+inputPassword.addEventListener('input', (e) => {
+  const divPassword = document.querySelector('[data-form="div-password"]')
+  if(e.target.value !== ''){
+    divPassword.classList.remove("input-login__user");
+    divPassword.classList.add("input-login__user--special")
+  }else{
+    divPassword.classList.remove("input-login__user--special");
+    divPassword.classList.add("input-login__user")
+  }
+})
