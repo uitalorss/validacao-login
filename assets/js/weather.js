b01a75fb1ac197dcc806cb4e618cd3b647ba31b0
Uítalo <uitalo.urss@gmail.com>
2022-10-17 15:01:46 -0300

    ajuste na exibição do nome da cidade e da sigla do estado

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,4 +1,6 @@
-  window.addEventListener("load", geolocation)
+  window.addEventListener("load", geolocation);
+  const infoArea = document.querySelector('[data-home="info-location-weather"]');
+
   
   function geolocation(){
     if(!('geolocation' in navigator)){
@@ -7,7 +9,9 @@
       navigator.geolocation.getCurrentPosition((position) => {
         let latitude = position.coords.latitude.toFixed(2)
         let longitude = position.coords.longitude.toFixed(2)
+        console.log(latitude, longitude)
         showLocation(latitude, longitude)
+        showCity(latitude, longitude);
       })
     }
   }
@@ -16,6 +20,14 @@
   const apiKey = 'key=b9cc4f5dfc484691a6e173148221610';
   const complement = "&q=";
 
+  function showCity(lat, long){
+    axios.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${long}&zoom=10&addressdetails=1`)
+    .then(function(response){
+      const infoCity = response.data;
+      showInfoCity(infoCity)
+    })
+  }
+
   function showLocation(lat, long){
     axios.get(`${baseUrl}${apiKey}${complement}${lat},${long}`)
     .then(function(response){
@@ -23,21 +35,24 @@
       showInfoWeather(infoWeather)
     })
   }
-  
+
   function showInfoWeather(response){
-    const infoArea = document.querySelector('[data-home="info-location-weather"]');
     const weatherArea = document.createElement("div");
-    let infoCity = document.createElement("p");
     let weather = document.createElement("h5");
     let icon = document.createElement("img");
     icon.src = response.current.condition.icon;
-    infoCity.classList.add("header-info-weather__city");
     weather.classList.add("header-info-weather__weather");
     weatherArea.classList.add("info-weather")
-    infoCity.innerHTML = response.location.name;
     weather.innerHTML = `${response.current.temp_c}°`;
-    infoArea.appendChild(infoCity);
     infoArea.appendChild(weatherArea);
     weatherArea.appendChild(icon)
     weatherArea.appendChild(weather);
+  }
+
+  function showInfoCity(response){
+    let infoState = response.address['ISO3166-2-lvl4'];
+    let infoCity = document.createElement("p");
+    infoCity.classList.add("header-info-weather__city");
+    infoCity.innerHTML = `${response.address.city} - ${infoState.substring(3, 5)}`;
+    infoArea.appendChild(infoCity);
   }
